fec2dff6fcb23b6d1bd176ee5d68cc6305a098e6
Manuel <mrctinio@gmail.com>
2021-02-09 20:54:47 +0800

    edit test for model

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -5,25 +5,30 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     get categories_path
     assert_response :success
   end
+
   test "2. Should get categories show" do
     activity = Category.create(title: 'Eats', description: 'Eat a bunch of foods', rating: 1)
     get show_category_path(title: activity.title)
     assert_response :success
   end
+
   test "3. Should get categories edit" do
     activity = Category.create(title: 'Eats', description: 'Eat a bunch of foods', rating: 1)
     get edit_category_path(title: activity.title)
     assert_response :success
   end
+
   test "4. Should post edited category" do
     activity = Category.create(title: 'Eats', description: 'Eat a bunch of foods', rating: 1)
     put update_category_path(title: activity.title), 
       params: { category: { title: "Eats", description: 'dont get hungry bitch', rating: 10} }
   end
+
   test "5. Should create new category" do
     get new_category_path
     assert_response :success
   end
+
   test "6. Should post new category" do
     assert_difference 'Category.count', 1 do
       post create_category_path, 
@@ -31,6 +36,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to categories_path
   end
+
   test "7. Should update category" do
     activity = Category.create(title: 'Eats', description: 'Eat a bunch of foods', rating: 1)
     put update_category_path(title: activity.title), 
@@ -38,6 +44,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     update = Category.find_by(title: 'Eats')
     assert update.description == 'dont get hungry bitch'
   end
+  
   test "8. Should delete category" do
     activity = Category.create(title: 'Eats', description: 'Eat a bunch of foods', rating: 1)
     assert_difference 'Category.count', -1 do
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,25 +1,25 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  test "1. does not save if title is missing" do
+  test "1. Should create with title" do
     category = Category.new
     category.rating = 10
     category.description = 'eating is fun and good'
     assert_not category.save, 'saved without title'
   end
 
-  test "2. does not save if rating is missing" do
+  test "2. Should create with rating" do
     category = Category.new
     category.title = 'Food'
     category.description = 'eating is fun and good'
-    assert_not category.save, 'saved without title'
+    assert_not category.save, 'saved without rating'
   end
 
-  test "3. does not save if description is too short" do
+  test "3. Should consider validations" do
     category = Category.new
     category.title = 'Food'
     category.rating = 10
     category.description = 'eat'
-    assert_not category.save, 'saved without title'
+    assert_not category.save, 'saved without validations'
   end
 end
